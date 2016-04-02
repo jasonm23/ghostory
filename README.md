ddf0d1d05ed0a8613ae0d91dd29d818679aef915
jasonm23 <jasonm23@gmail.com>
2016-04-02 14:46:20 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ghostory
+
+Once upon a time, blablabla...
---
+++ src/jasonm23.ghostory/ghostory/main.go
@@ -0,0 +1,10 @@
+package main
+
+import(
+"fmt"
+"jasonm23.ghostory/ghostory/hello"
+) 
+
+func main() {
+	fmt.Println(hello.BuildHello())
+}
